ba54149977f0c84be2f395f828085d9d901b13f7
Jason Fu <jasonfu1992@gmail.com>
2019-04-10 13:16:21 -0700

    Add Bcrypt password

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  return res.json({msg: "This is the users route"});
+// Import bcrypt
+const bcrypt = require("bcryptjs");
+
+// Import User model
+const User = require("../../models/User");
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "A user has already been registered with this email." });
+      }
+      else {
+        const newUser = new User({
+          email: req.body.email,
+          username: req.body.username,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
 });
 
 module.exports = router;
