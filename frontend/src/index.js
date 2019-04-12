db9e8171888422a3b8a9f0cd56093802655addb4
Jason Fu <jasonfu1992@gmail.com>
2019-04-11 20:57:14 -0700

    Refactor store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,39 +1,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Root from './components/root';
-
-import configureStore from './store/store';
-
-import jwt_decode from 'jwt-decode';
-
-import { setAuthToken } from './util/sessionApiUtil';
-
-import { logout } from './actions/sessionActions';
+import setCurrentUser from './util/setCurrentUser';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store;
-  if (localStorage.jwtToken) {
-    setAuthToken(localStorage.jwtToken);
-
-    const decodedUser = jwt_decode(localStorage.jwtToken);
-
-    const preloadedState = { session: {isAuthenticated: true, user: decodedUser }};
-
-    store = configureStore(preloadedState);
-
-    const currenttime = Date.now() / 1000;
-
-    if (decodedUser.exp < currenttime) {
-      store.dispatch(logout());
-      window.location.href = '/login';
-    }
-  }
-  else {
-    store = configureStore();
-  }
-
+  const store = setCurrentUser();
   const root = document.getElementById('root');
+  
   ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/src/util/setCurrentUser.js
@@ -0,0 +1,28 @@
+import { setAuthToken } from './sessionApiUtil';
+import jwt_decode from 'jwt-decode';
+import { logout } from '../actions/sessionActions';
+import configureStore from '../store/store';
+
+const setCurrentUser = () => {
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
+    const store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+
+    return store;
+  }
+  else {
+    return configureStore();
+  }
+};
+
+export default setCurrentUser;
